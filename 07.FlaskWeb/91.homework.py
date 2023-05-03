b4da89e61eab7dc7e04083fbdbc82217957562b6
Myungho Lee <su757202@gmail.com>
2023-05-03 17:37:48 +0900

    2023

--- 07.FlaskWeb/91.homework.py
+++ 07.FlaskWeb/91.homework.py
@@ -1,38 +1,31 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
+import graph_util as gu
 import map_util as mu
 import crawl_util as cu
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('14.carousel.html')
+def home():
+    return render_template('homework/home.html')
 
 @app.route('/scatter', methods=['GET','POST'])
 def scatter():
     if request.method == 'GET':
-        return render_template('09.산점도.html')
+        return render_template('homework/scatter.html')
     else:
         num = int(request.form['num'])
         mean = float(request.form['mean'])
         std = float(request.form['std'])
         min = float(request.form['min'])
         max = float(request.form['max'])
-        xs = np.random.normal(loc=mean, scale=std, size=num)
-        ys = np.random.uniform(min, max, num)
-        plt.figure()
-        plt.scatter(xs, ys)
-        filename = os.path.join(app.static_folder, 'img/scatter.png')
-        plt.savefig(filename)
-        return render_template('09.산점도_res.html')
+        gu.scatter(num, mean, std, min, max, app)
+        return render_template('homework/scatter_res.html')
 
 @app.route('/hotPlaces', methods=['GET','POST'])
-def hotPlaces():
+def hot_places():
     if request.method == 'GET':
-        return render_template('10.hotplaces.html')
+        return render_template('homework/hot_places.html')
     else:
         # client가 입력한 장소 알아내기
         place1 = request.form['place1']
@@ -40,16 +33,16 @@ def hotPlaces():
         place3 = request.form['place3']
         places = [place1, place2, place3]
         mu.hot_places(places, app)
-        return render_template('10.hotplaces_res.html')
+        return render_template('homework/hot_places_res.html')
 
 @app.route('/interpark')
 def interpark():
     book_list = cu.interpark()
-    return render_template('11.interpark.html', book_list=book_list)
+    return render_template('homework/interpark.html', book_list=book_list)
 
 @app.route('/progress')
 def progress():
-    return render_template('13.progress bar.html')
+    return render_template('homework/progress_bar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

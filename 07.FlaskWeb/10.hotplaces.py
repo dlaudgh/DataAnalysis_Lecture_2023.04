20a9c546901d25d8cc9806c57996c3a03ea70bc3
Myungho Lee <su757202@gmail.com>
2023-05-03 10:04:54 +0900

    2023.05.03

---
+++ 07.FlaskWeb/10.hotplaces.py
@@ -0,0 +1,66 @@
+import folium, json, requests, os
+import pandas as pd
+from urllib.parse import quote
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Flask'
+
+@app.route('/hotPlaces', methods=['GET','POST'])
+def hotPlaces():
+    if request.method == 'GET':
+        return render_template('10.hotplaces.html')
+    else:
+        # client가 입력한 장소 알아내기
+        place1 = request.form['place1']
+        place2 = request.form['place2']
+        place3 = request.form['place3']
+        places = [place1, place2, place3]
+
+        # 도로명주소 구하기
+        with open('../04.지도시각화/data/roadapikey.txt') as f:
+            road_key = f.read()
+        base_url = 'https://www.juso.go.kr/addrlink/addrLinkApiJsonp.do'
+        params1 = f'confmKey={road_key}&currentPage=1&countPerPage=10&resultType=json'
+        road_addr_list = []
+        for place in places:
+            url = f'{base_url}?{params1}&keyword={quote(place)}'
+            result = requests.get(url)
+            if result.status_code == 200:
+                res = json.loads(result.text[1:-1])
+                road_addr_list.append(res['results']['juso'][0]['roadAddr'])
+            else:
+                print(result.status_code)
+        df = pd.DataFrame({'place':places, 'addr':road_addr_list})
+
+        # 위도, 경도 좌표 구하기
+        with open('../04.지도시각화/data/kakaoapikey.txt') as f:
+            kakao_key = f.read()
+        base_url = 'https://dapi.kakao.com/v2/local/search/address.json'
+        header = {'Authorization': f'KakaoAK {kakao_key}'}
+        lat_list, lng_list = [], []
+        for i in df.index:
+            url = f'{base_url}?query={quote(df.addr[i])}'
+            result = requests.get(url, headers=header).json()
+            lat_list.append(float(result['documents'][0]['y']))
+            lng_list.append(float(result['documents'][0]['x']))
+        df['위도'] = lat_list
+        df['경도'] = lng_list
+
+        # map 그리기
+        map = folium.Map(location=[df.위도.mean(), df.경도.mean()], zoom_start=11)
+        for i in df.index:
+            folium.Marker(
+                location=[df.위도[i], df.경도[i]],
+                popup=folium.Popup(df.addr[i], max_width=200),
+                tooltip=df.place[i], 
+            ).add_to(map)
+        filename = os.path.join(app.static_folder, 'img/hotplaces.html')
+        map.save(filename)
+        return render_template('10.hotplaces_res.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

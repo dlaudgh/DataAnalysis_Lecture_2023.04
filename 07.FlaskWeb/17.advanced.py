4f1f3f92278b4e7c9465ca3fa4075940a54da20f
Myungho Lee <su757202@gmail.com>
2023-05-08 10:16:52 +0900

    2023

--- 07.FlaskWeb/17.advanced.py
+++ 07.FlaskWeb/17.advanced.py
@@ -1,43 +1,55 @@
 from flask import Flask, render_template, request
 from weather_util import get_weather
 import crawl_util as cu
+import os, random
 
 app = Flask(__name__)
+
+@app.before_first_request
+def before_first_request():
+    global quote       # quotes 변수를 전역 변수로 만들어 줌
+    filename = os.path.join(app.static_folder, 'data/todayQuote.txt')
+    with open(filename, encoding='utf-8') as f:
+        quotes = f.readlines()
+    quote = random.sample(quotes, 1)[0]
 
 @app.route('/')
 def home():
     menu = {'ho':1, 'us':0, 'cr':0, 'sc':0}
-    return render_template('prototype/home.html', menu=menu, weather=get_weather(app))
+    return render_template('prototype/home.html', menu=menu, weather=get_weather(app),
+                           quote=quote)
 
 @app.route('/interpark')
 def interpark():
     menu = {'ho':0, 'us':0, 'cr':1, 'sc':0}
     book_list = cu.interpark()
     return render_template('prototype/interpark.html', book_list=book_list, 
-                           menu=menu, weather=get_weather(app))
+                           menu=menu, weather=get_weather(app), quote=quote)
 
 @app.route('/genie')
 def genie():
     menu = {'ho':0, 'us':0, 'cr':1, 'sc':0}
     song_list = cu.genie()
     return render_template('prototype/genie.html', song_list=song_list, 
-                           menu=menu, weather=get_weather(app))
+                           menu=menu, weather=get_weather(app), quote=quote)
 
 @app.route('/siksin', methods=['GET', 'POST'])
 def siksin():
     menu = {'ho':0, 'us':0, 'cr':1, 'sc':0}
     if request.method == 'GET':
-        return render_template('prototype/siksin.html', menu=menu, weather=get_weather(app))
+        return render_template('prototype/siksin.html', menu=menu, weather=get_weather(app),
+                               quote=quote)
     else:
         place = request.form['place']
         food_list = cu.siksin(place)
         return render_template('prototype/siksin_res.html', food_list=food_list, 
-                               menu=menu, weather=get_weather(app), place=place)
+                               menu=menu, weather=get_weather(app), place=place, quote=quote)
 
 @app.route('/schedule')
 def schedule():
     menu = {'ho':0, 'us':0, 'cr':0, 'sc':1}
-    return render_template('prototype/schedule.html', menu=menu, weather=get_weather(app))
+    return render_template('prototype/schedule.html', menu=menu, weather=get_weather(app),
+                           quote=quote)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 07.FlaskWeb/crawl_util.py
+++ 07.FlaskWeb/crawl_util.py
@@ -1,6 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
 from urllib.parse import quote
+from bs4 import BeautifulSoup
 
 def interpark():
     base_url = 'http://book.interpark.com'
@@ -8,7 +8,7 @@ def interpark():
     res = requests.get(base_url + sub_url)
     soup = BeautifulSoup(res.text, 'html.parser')
     lis = soup.select('.rankBestContentList > ol > li')
-    books = []
+    lines = []
     for li in lis:
         img = li.select_one('.coverImage').find('img')['src']
         href = li.select_one('.coverImage').find('a')['href']
@@ -16,14 +16,14 @@ def interpark():
         if len(rank_data) == 1:
             rank = int(rank_data[0]['class'][-1][-1])
         else:
-            rank = int(rank_data[0]['class'][-1][-1] + rank_data[1]['class'][-1][-1])
+            rank = int(rank_data[0]['class'][-1][-1] + rank_data[1]['class'][-1][-1]) 
         title = li.select_one('.itemName').get_text().strip()
         author = li.select_one('.author').get_text().strip()
         company = li.select_one('.company').get_text().strip()
-        price = li.select_one('.price > em').get_text().strip()
-        books.append({'순위':rank,'제목':title,'저자':author,'출판사':company,
-                      '가격':price,'img':img,'href':base_url+href})
-    return books
+        price = li.select_one('.price > em').get_text().strip()   
+        lines.append({'순위':rank, '제목':title, '저자':author, '출판사':company, 
+                      '가격':price, 'img':img, 'href':base_url+href})
+    return lines
 
 def genie():
     url = 'https://www.genie.co.kr/chart/top200'
@@ -31,23 +31,23 @@ def genie():
     result = requests.get(url, headers=header)
     soup = BeautifulSoup(result.text, 'html.parser')
     trs = soup.select('tr.list')
-    chart = []
+    lines = []
     for tr in trs:
         rank = tr.select_one('.number').get_text().split('\n')[0].strip()
-        img = 'http:' + tr.select_one('.cover > img')['src']
+        img = 'https:' + tr.select_one('.cover > img')['src']
         title = tr.select_one('.title.ellipsis').get_text().split('\n')[-1].strip()
         artist = tr.select_one('.artist.ellipsis').string.strip()
         album = tr.select_one('.albumtitle.ellipsis').text.strip()
-        chart.append({'rank':rank,'img':img,'title':title,'artist':artist,'album':album})
-    return chart
+        lines.append({'rank':rank, 'img':img, 'title':title, 'artist':artist, 'album':album}) 
+    return lines
 
-def siksin():
+def siksin(place):
     base_url = 'https://www.siksinhot.com/search'
-    url = f'{base_url}?keywords={quote("장안문")}'
+    url = f'{base_url}?keywords={quote(place)}'
     result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
     lis = soup.select('.localFood_list > li')
-    siksin = []
+    lines = []
     for li in lis:
         img = li.find('img')['src']
         title = li.select_one('.textBox > h2').get_text()
@@ -55,5 +55,5 @@ def siksin():
         atags = li.select('.cate > a')
         location = atags[0].get_text()
         menu = atags[1].get_text()
-        siksin.append({'img':img,'title':title,'score':score,'location':location,'menu':menu})
-    return siksin
+        lines.append({'img':img, 'title':title, 'score':score, 'location':location, 'menu':menu})
+    return lines

cf6cf77902eabf49806d10807458a2fd02a1dbac
Myungho Lee <su757202@gmail.com>
2023-05-09 12:48:06 +0900

    2023

--- 07.FlaskWeb/18.session.py
+++ 07.FlaskWeb/18.session.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from weather_util import get_weather, get_weather_by_coord
 import crawl_util as cu
 import map_util as mu
 import image_util as iu
-import os, random
+import os, random, json
+from user import user_bp
+# from user_module.user import user_bp
 
 app = Flask(__name__)
+app.secret_key = 'qwert12345'
+app.config['SESSION_COOKIE_PATH'] = '/'
+
+app.register_blueprint(user_bp, url_prefix='/user')
+
+# flask 2.3 에서는 이 코드만 사용 가능
+# with app.app_context():
+#     global quote, quotes            # quote, quotes 변수를 전역 변수로 만들어 줌
+#     global addr
+#     filename = os.path.join(app.static_folder, 'data/todayQuote.txt')
+#     with open(filename, encoding='utf-8') as f:
+#         quotes = f.readlines()
+#     quote = random.sample(quotes, 1)[0]
+#     session['quote'] = quote
+#     addr = '수원시 장안구'
 
 @app.before_first_request
 def before_first_request():
@@ -15,19 +32,23 @@ def before_first_request():
     with open(filename, encoding='utf-8') as f:
         quotes = f.readlines()
     quote = random.sample(quotes, 1)[0]
+    session['quote'] = quote
     addr = '수원시 장안구'
+    session['addr'] = addr
 
 # for AJAX #############################
 @app.route('/change_quote')
 def change_quote():
     global quote
     quote = random.sample(quotes, 1)[0]
+    session['quote'] = quote
     return quote
 
 @app.route('/change_addr')
 def change_addr():
     global addr
     addr = request.args.get('addr')
+    session['addr'] = addr
     return addr
 
 @app.route('/weather')
@@ -53,10 +74,12 @@ def home():
     return render_template('prototype/home.html', menu=menu, weather=get_weather(app),
                            quote=quote, addr=addr)
 
-@app.route('/user')
-def user():
-    menu = {'ho':0, 'us':1, 'cr':0, 'sc':0}
-    return redirect('/schedule')
+
+
+# @app.route('/user')
+# def user():
+#     menu = {'ho':0, 'us':1, 'cr':0, 'sc':0}
+#     return redirect('/schedule')
 
 @app.route('/interpark')
 def interpark():
@@ -86,6 +109,12 @@ def siksin():
 
 @app.route('/schedule')
 def schedule():
+    try:
+        _ = session['uid']
+    except:
+        flash('스케쥴을 확인하려면 로그인하여야 합니다.')
+        return redirect('/user/login')
+    
     menu = {'ho':0, 'us':0, 'cr':0, 'sc':1}
     return render_template('prototype/schedule.html', menu=menu, weather=get_weather(app),
                            quote=quote, addr=addr)
